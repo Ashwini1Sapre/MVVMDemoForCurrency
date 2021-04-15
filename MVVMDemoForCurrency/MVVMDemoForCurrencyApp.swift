80cf1498d58e504551c9a1f1680b9d9230a5fddf
ashwinisapreknoxpo <ashwini.sapre@knoxpo.com>
2021-04-15 10:33:07 +0530

    Initial Commit

---
+++ MVVMDemoForCurrency/MVVMDemoForCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMDemoForCurrencyApp.swift
+//  MVVMDemoForCurrency
+//
+//  Created by Knoxpo MacBook Pro on 15/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MVVMDemoForCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
